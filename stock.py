5d9061e011f291431bc833ea2c3ee8a54461ab50
Jean Roelens <jeanroelens31@gmail.com>
2024-04-27 20:40:52 +0200

    ex3_1 finished

--- stock.py
+++ stock.py
@@ -1,4 +1,5 @@
 # stock.py
+import csv
 
 
 class Stock:
@@ -12,3 +13,27 @@ class Stock:
 
     def sell(self, nshares):
         self.shares -= nshares
+
+
+def read_portfolio(filename):
+    resp = []
+    with open(filename, "r") as f:
+        rows = csv.reader(f)
+        headers = next(rows)
+        for row in rows:
+            S = Stock(row[0], int(row[1]), float(row[2]))
+            resp.append(S)
+    return resp
+
+
+def print_portfolio(portfolio):
+    print("%10s %10s %10s" % ("name", "shares", "price"))
+    print(("-" * 10 + " ") * 3)
+    for row in portfolio:
+        print("%10s %10d %10.2f" % (row.name, row.shares, row.price))
+
+
+portfolio = read_portfolio("Data/portfolio.csv")
+for s in portfolio:
+    print(repr(s))
+print_portfolio(portfolio=portfolio)
