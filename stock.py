d6205fd943520cc1b1a061c2e1cf69068a7dc15e
Jean Roelens <jeanroelens31@gmail.com>
2024-05-03 23:01:10 +0200

    3_3 Done

--- stock.py
+++ stock.py
@@ -1,12 +1,20 @@
 # stock.py
 import csv
+from decimal import Decimal
 
 
 class Stock:
+    types = (str, int, Decimal)
+
     def __init__(self, name, shares, price):
         self.name = name
         self.shares = shares
         self.price = price
+
+    @classmethod
+    def from_row(cls, row):
+        values = [func(val) for func, val in zip(cls.types, row)]
+        return cls(*values)
 
     def cost(self):
         return self.shares * self.price
@@ -15,25 +23,8 @@ class Stock:
         self.shares -= nshares
 
 
-def read_portfolio(filename):
-    resp = []
-    with open(filename, "r") as f:
-        rows = csv.reader(f)
-        headers = next(rows)
-        for row in rows:
-            S = Stock(row[0], int(row[1]), float(row[2]))
-            resp.append(S)
-    return resp
-
-
 def print_portfolio(portfolio):
     print("%10s %10s %10s" % ("name", "shares", "price"))
     print(("-" * 10 + " ") * 3)
     for row in portfolio:
         print("%10s %10d %10.2f" % (row.name, row.shares, row.price))
-
-
-# portfolio = read_portfolio("Data/portfolio.csv")
-# for s in portfolio:
-#     print(repr(s))
-# print_portfolio(portfolio=portfolio)
