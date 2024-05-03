0eab6267b80b9c5c99627f41b1b87ed404b59dd5
Jean Roelens <jeanroelens31@gmail.com>
2024-05-03 23:27:27 +0200

    3_4 Done

--- stock.py
+++ stock.py
@@ -4,7 +4,8 @@ from decimal import Decimal
 
 
 class Stock:
-    types = (str, int, Decimal)
+    _types = (str, int, float)
+    __slots__ = ["name", "_shares", "_price"]
 
     def __init__(self, name, shares, price):
         self.name = name
@@ -16,11 +17,40 @@ class Stock:
         values = [func(val) for func, val in zip(cls.types, row)]
         return cls(*values)
 
+    @property
     def cost(self):
         return self.shares * self.price
 
+    @property
+    def shares(self):
+        return self.shares
+
+    @shares.setter
+    def shares(self, val):
+        if not isinstance(val, int):
+            raise TypeError("Expected an integer")
+        if val < 0:
+            raise ValueError("Shares must be >= 0")
+        self._shares = val
+
+    @property
+    def price(self):
+        return self.price
+
+    @price.setter
+    def price(self, val):
+        if not isinstance(val, float):
+            raise TypeError("Expected a float")
+        if val < 0:
+            raise ValueError("Price must be >= 0")
+        self._price = val
+
     def sell(self, nshares):
         self.shares -= nshares
+
+
+class DStock(Stock):
+    _types = (str, int, Decimal)
 
 
 def print_portfolio(portfolio):
