8f950b653e98986f24346c2f21e85fb28bc1f142
Jean Roelens <jeanroelens31@gmail.com>
2024-05-23 23:56:01 +0200

    Section 3 done

--- tableformat.py
+++ tableformat.py
@@ -1,6 +1,15 @@
 from abc import ABC, abstractmethod
 
 
+def print_table(records, fields, formatter):
+    if not isinstance(formatter, TableFormatter):
+        raise RuntimeError("Expected a TableFormatter")
+    formatter.headings(fields)
+    for r in records:
+        rowdata = [getattr(r, fieldname) for fieldname in fields]
+        formatter.row(rowdata)
+
+
 class TableFormatter(ABC):
     @abstractmethod
     def headings(self, headers):
@@ -22,46 +31,57 @@ class TextTableFormatter(TableFormatter):
 
 class CSVTableFormatter(TableFormatter):
     def headings(self, headers):
-        print("".join("%s," % val for val in headers))
+        print(",".join(headers))
 
     def row(self, rowdata):
-        print("".join("%s," % d for d in rowdata))
+        print(",".join(str(d) for d in rowdata))
 
 
 class HTMLTableFormatter(TableFormatter):
     def headings(self, headers):
         print("<tr>", end=" ")
-        print(" ".join("<td>%s</td>" % val for val in headers), end=" ")
+        for h in headers:
+            print("<th>%s</th>" % h, end=" ")
         print("</tr>")
 
     def row(self, rowdata):
         print("<tr>", end=" ")
-        print(" ".join("<td>%s</td>" % val for val in rowdata), end=" ")
+        for d in rowdata:
+            print("<td>%s</td>" % d, end=" ")
         print("</tr>")
 
 
-def print_table(records, fields, formatter):
-    if not isinstance(formatter, TableFormatter):
-        raise TypeError(" Expected a TableFormatter")
-    formatter.headings(fields)
-    for r in records:
-        rowdata = [getattr(r, fieldname) for fieldname in fields]
-        formatter.row(rowdata)
-
-
-def create_formatter(name):
-    if name == "html":
-        return HTMLTableFormatter()
-    if name == "text":
-        return TextTableFormatter()
-    if name == "csv":
-        return CSVTableFormatter()
-    raise RuntimeError("Unknown format %s" % name)
-
-
-class NewFormatter(TableFormatter):
-    def headers(self, headings):
-        pass
+class ColumnFormatMixin:
+    formats = []
 
     def row(self, rowdata):
-        pass
+        rowdata = [(fmt % item) for fmt, item in zip(self.formats, rowdata)]
+        super().row(rowdata)
+
+
+class UpperHeadersMixin:
+    def headings(self, headers):
+        super().headings([h.upper() for h in headers])
+
+
+def create_formatter(name, column_formats=None, upper_headers=False):
+    if name == "text":
+        formatter_cls = TextTableFormatter
+    elif name == "csv":
+        formatter_cls = CSVTableFormatter
+    elif name == "html":
+        formatter_cls = HTMLTableFormatter
+    else:
+        raise RuntimeError("Unknown format %s" % name)
+
+    if column_formats:
+
+        class formatter_cls(ColumnFormatMixin, formatter_cls):
+            formats = column_formats
+
+    if upper_headers:
+
+        class formatter_cls(UpperHeadersMixin, formatter_cls):
+            pass
+
+    return formatter_cls()
